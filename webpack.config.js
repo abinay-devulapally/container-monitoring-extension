154cb4f2476dca40b78696144feb0bc2347e9982
abinay-devulapally <abinaykumar.d99@gmail.com>
2024-08-07 11:50:45 -0400

    bundle extension file

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+
+module.exports = {
+  target: "node", // since this is a VSCode extension
+
+  entry: "./extension.js", // Adjust the entry point to extension.js
+
+  output: {
+    filename: "extension.bundle.js", // Output bundle name
+    path: path.resolve(__dirname, "out"),
+    libraryTarget: "commonjs2",
+  },
+
+  externals: [nodeExternals()], // Exclude node_modules
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+};
